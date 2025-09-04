4cfee03e583d85e2af9b98098be9b88efb7239d9
Naveen pandian <npandian515@gmail.com>
2025-09-04 19:04:47 +0530

    tansatcak is dipped in

--- switch-pay/src/main.jsx
+++ switch-pay/src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {WagmiProvider} from "wagmi"
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+
 import './index.css'
 import App from './App.jsx'
 
