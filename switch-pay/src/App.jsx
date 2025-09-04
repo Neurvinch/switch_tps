88cc8820f00d1860dc77893d4101b9fcf64ab066
Naveen pandian <npandian515@gmail.com>
2025-09-04 19:51:34 +0530

    wallet button been plotted

--- switch-pay/src/App.jsx
+++ switch-pay/src/App.jsx
@@ -1,4 +1,5 @@
 
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 import './App.css'
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-      
+      <ConnectButton/>
     </>
   )
 }
