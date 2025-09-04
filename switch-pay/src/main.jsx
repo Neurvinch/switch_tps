2697e61cefc0f3708c35920f4ff2e21e01cc9ec8
Naveen pandian <npandian515@gmail.com>
2025-09-04 19:48:38 +0530

    wallet config big piece done

--- switch-pay/src/main.jsx
+++ switch-pay/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import {WagmiProvider} from "wagmi"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import { http } from 'wagmi'
-import {sonicTestnet} from "viem/chains"
+import {sonicBlazeTestnet} from "viem/chains"
 import {RainbowKitProvider, darkTheme,getDefaultConfig} from "@rainbow-me/rainbowkit"
 import "@rainbow-me/rainbowkit/styles.css "
 import './index.css'
@@ -13,6 +13,16 @@ import App from './App.jsx'
  const projectId = "8f701441da6e625d8a8be7f9e002437e"
 
  const queryClient = new  QueryClient()
+
+ const config = getDefaultConfig({
+   appName : "SwitchPay",
+  projectId,
+  chains:[sonicBlazeTestnet],
+  transports: {
+    [sonicBlazeTestnet.id] : http("https://worldchain-sepolia.g.alchemy.com/v2/bBA5Mnb4SmI254K3uqriS"),
+
+  }
+ })
 
   const theme = darkTheme({
       accentColor: '#7b3fe4',
