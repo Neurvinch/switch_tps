e0ceed7e361cac988412bb84f30b437aa990da75
Naveen pandian <npandian515@gmail.com>
2025-09-05 10:10:29 +0530

    Initialize Express server with CORS and JSON middleware

--- backend/index.js
+++ backend/index.js
@@ -5,3 +5,11 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get("/",(req,res) => {
+    res.json({ message: "Sonic StreamPay Backend is running" });
+})
+
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+})
