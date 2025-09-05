f0b4a336441770eec36730d285376e0d135343c1
Naveen pandian <npandian515@gmail.com>
2025-09-05 17:33:05 +0530

    Implement stream creation functionality in App component

--- switch-pay/src/App.jsx
+++ switch-pay/src/App.jsx
@@ -1,9 +1,34 @@
 
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import './App.css'
-import {} from "./api"
+import {createStream, withdraw} from "./api"
+import { useState } from 'react';
 
 function App() {
+  const [recipient, setRecipient] =  useState("");
+  const [rate, setRate] = useState("");
+  const [duration , setDuration] = useState("");
+  const [streamID, setStreamID] = useState("");
+
+  const handleCreateStream = async () => {
+
+      try {
+
+        const res = await createStream(recipient, rate, duration);
+        alert(`Stream created! Tx Hash: ${res.data.txHash}`);
+        setStreamID(1)
+        
+      } catch (error) {
+        
+        console.error("Error creating stream:", error);
+        alert("Failed to create stream");
+      }
+  }
+
+
+  const handleWithdraww = async () => {
+    
+  }
   
 
   return (
